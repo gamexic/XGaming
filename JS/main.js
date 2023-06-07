f5dcbc4c40637c3b00818f5e0f8d20acf600d94f
Frank Alayza <stephanoherrera4444@gmail.com>
2023-06-07 00:50:57 -0500

    Adding the Header

--- JS/main.js
+++ JS/main.js
@@ -10,6 +10,37 @@ select_memory(main_product_memory_256gb, main_product_memory_512gb);
 select_memory(main_product_memory_512gb, main_product_memory_64gb);
 select_memory(main_product_memory_512gb, main_product_memory_256gb);
 
+/* Nav Bar Hamburger */
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#141414';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+/* End Nav Bar Hamburguer */
+
 // Main Product Price Section
 
 const main_product_price_offer = document.getElementById("main-product-price_offer");
